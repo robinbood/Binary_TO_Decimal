d1cbd7a7c8b4eb3893bd0f98b45858ffd0a464fd
robinbood <mercedesfarts@gmail.com>
2025-04-26 11:09:53 +0500

    fixxed the issue

--- src/App.tsx
+++ src/App.tsx
@@ -12,50 +12,51 @@ const App = () => {
   const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault()
     const message = value.match(/^[01]+$/)
+    if (!value) {
+      setError("Please enter a value")
+    }
     if (!message) {
       setError("  0s and 1s only #_#")
       setTimeout(() => {
         setError("Come On! It's just 0s and 1s")
       }, 4000)
-      return
+      setTimeout(() => {
+        setError("Do better ay")
+      }, 7000)
+
     }
-   
-    const digit:number = value.split('').map(Number).reverse().reduce((acc, curr,index ) :number => { return acc + curr * Math.pow(2,index) })
+
+    const digit: number = value.split('').map(Number).reverse().reduce((acc, curr, index): number => { return acc + curr * Math.pow(2, index) })
     setDecimal(digit)
-    
-
-
-    
-    
   }
 
 
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <h3>{error}</h3>
-        </div>
-        <div>
-          <h4>binary system works from <em>right to left</em> 1 means a value and 0 means otherwise. The <q> Values</q>  are then calculated from the index and at each index the value is two times the previous one</h4>
-        </div>
-        
-        <h2>Practice Binary By Practicing :D</h2>
-        <em>it's just  0s and 1s after all </em>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            value={value}
-            onChange={({ target }) => setValue(target.value)}
-            placeholder="Enter a number"
-          />
-          <button type="submit">Submit</button>
-        </form>
-        <div>
-          <h3>Binary: {decimal}</h3>
-        </div>
-        
+        <h3>{error}</h3>
       </div>
-    )
+      <div>
+        <h4>binary system works from <em>right to left</em> 1 means a value and 0 means otherwise. The <q> Values</q>  are then calculated from the index and at each index the value is two times the previous one</h4>
+      </div>
+
+      <h2>Practice Binary By Practicing :D</h2>
+      <em>it's just  0s and 1s after all </em>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={value}
+          onChange={({ target }) => setValue(target.value)}
+          placeholder="Enter a number"
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <div>
+        <h3>Binary: {decimal}</h3>
+      </div>
+
+    </div>
+  )
 }
 
 
